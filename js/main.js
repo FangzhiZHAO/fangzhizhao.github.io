949fb23b620c8c0e63f49946177bf639072f56bb
Fangzhi <fangzhi@MC-Air.local>
2016-09-21 18:59:07 -0500

    Animation works. Also add a star.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
-TweenLite.defaultEase = Linear.easeNone;
-var tl = new TimelineLite()
+$(document).ready(function() {
+    animateDiv();
 
-tl.to(".box b", 4.05, {x:1000, repeat:-1, yoyo:true})
-  .to(".box b", 8.4, {y:720, repeat:-1, yoyo:true}, 0)
+});
+
+function makeNewPosition($container) {
+
+    // Get viewport dimensions (remove the dimension of the div)
+    $container = ($container || $(window))
+    var h = $container.height() - 50;
+    var w = $container.width() - 50;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+
+}
+
+function animateDiv() {
+    var $target = $('.a');
+    var newq = makeNewPosition($target.parent());
+    var oldq = $target.offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+    $('.a').animate({
+        top: newq[0],
+        left: newq[1]
+    }, speed, function() {
+        animateDiv();
+    });
+
+};
+
+function calcSpeed(prev, next) {
+
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+
+    var greatest = x > y ? x : y;
+
+    var speedModifier = 3.3;
+
+    var speed = Math.ceil(greatest / speedModifier);
+
+    return speed;
+
+}
